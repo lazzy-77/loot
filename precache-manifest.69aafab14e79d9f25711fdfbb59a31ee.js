a656851b2c8b10b99cfad8d50ac9791a44992861
lazzy-77 <45042801+lazzy-77@users.noreply.github.com>
2020-09-30 23:27:40 -0500

    Updates

---
+++ precache-manifest.69aafab14e79d9f25711fdfbb59a31ee.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b077e584360fd0579bcf2a08fb5c8cd6",
+    "url": "/loot/index.html"
+  },
+  {
+    "revision": "6ee3cf92146a55639184",
+    "url": "/loot/static/css/main.b211f17e.chunk.css"
+  },
+  {
+    "revision": "0fca8a38743770a8505a",
+    "url": "/loot/static/js/2.a719d4e5.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/loot/static/js/2.a719d4e5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6ee3cf92146a55639184",
+    "url": "/loot/static/js/main.db2148a3.chunk.js"
+  },
+  {
+    "revision": "dc14b6b379a903970ae7",
+    "url": "/loot/static/js/runtime-main.e001b3cb.js"
+  }
+]);
